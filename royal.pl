119898d92e2d941ab50c313dba43e8321c0d57dd
runner15 <therunner151@gmail.com>
2018-04-24 13:06:12 -0600

    Finished lab with updated royal

--- royal.pl
+++ royal.pl
@@ -81,7 +81,7 @@ married('King Edward VII', 'Princess Alexandra of Denmark').
 married('King George V', 'Queen Mary').
 married('King Edward VIII', 'Mrs Simpson').
 married('King George VI', 'Lady Elizabeth Bowes-Lyon').
-married('Queen Elizabeth II', 'Prince Philip').
+married('Prince Philip', 'Queen Elizabeth II').
 married('Prince Charles', 'Lady Diana Spencer').
 married('Prince William', 'Catherine Middleton').
 married('Antony Armstrong-Jones', 'Princess Margaret').
